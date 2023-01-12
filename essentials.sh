de68636397c3071e9b7a8a0e896a29028aabdfc9
Kirill Fedtsov <110291801+IdoubledareU31@users.noreply.github.com>
2023-01-12 14:50:56 +0300

    Update essentials.sh

--- essentials.sh
+++ essentials.sh
@@ -2,6 +2,7 @@
 
 # packages
 sudo yum install gcc openssl-devel bzip2-devel libffi-devel zlib-devel
+sudo yum update -y
 
 # python3.9.1
 wget https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz
@@ -13,4 +14,28 @@ python3.9 -V
 
 # pipreqs to generate requirements.txt
 pip install pipreqs
+
+# vim
+sudo yum install vim
+
+# install git
+wget https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.39.0.tar.gz
+tar xf git-2.39.0.tar.gz
+cd git-2.39.0/
+
+yum groupinstall 'Development Tools'
+yum install curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-CPAN perl-devel
+
+make configure
+./configure --prefix=/usr/local
+
+make all
+make install
+
+# telegram
+pip install pytelegrambotapi aiohttp aiogram
+
+# install fail2ban and start it with "fail2ban-client start" edit the config int /etc/fail2ban/jail.local (crate a copy of jail.conf)
+
+yum install fail2ban
 
